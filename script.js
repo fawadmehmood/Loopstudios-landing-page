112d2899b0d86c9ff39eaba96520b49dea3b4ee9
fawadmehmood <fawadmehmood10@gmail.com>
2021-03-06 17:48:16 +0500

    fixed the navigation menu for smaller screens

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const hamburgerMenu = document.querySelector(".hamburger__menu");
-const header = document.querySelector(".header");
-const headerNav = document.querySelector(".header_nav");
+const headerWrapper = document.querySelector(".header__wrapper");
 const body = document.querySelector("body");
 
 document.body.onresize = detectWindow;
 
     function detectWindow() {
-        if (window.screen.width > 768 && headerNav.classList.contains("hide__ham")) {
-            headerNav.classList.remove("hide__ham");
+        if (window.screen.width > 768 && headerWrapper.classList.contains("hide__mob__nav")) {
+            headerWrapper.classList.remove("hide__mob__nav");
         }
     }
 
@@ -17,13 +16,14 @@ hamburgerMenu.addEventListener("click", openHamburger)
 function openHamburger() {
     hamburgerMenu.classList.toggle("open__hambuger");
     if (hamburgerMenu.classList.contains("open__hambuger")) {
-        headerNav.classList.add("display__ham");
-        headerNav.classList.remove("hide__ham");
         body.classList.add("noscroll");
+        headerWrapper.classList.remove("hide__mob__nav");
+        headerWrapper.classList.add("display__mob__nav");
     }
     else {
-        headerNav.classList.remove("display__ham");
-        headerNav.classList.add("hide__ham");
         body.classList.remove("noscroll");
+        headerWrapper.classList.remove("display__mob__nav");
+        headerWrapper.classList.add("hide__mob__nav");
+        
     }
 }
